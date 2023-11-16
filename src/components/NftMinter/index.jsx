528e31a26eaf058a85c96570663d41f80d37bce0
kimkim <nkimura2021infotech@gmail.com>
2023-11-16 23:56:07 +0900

    First commit

--- src/components/NftMinter/index.jsx
+++ src/components/NftMinter/index.jsx
@@ -1,15 +1,45 @@
 import { Button } from "@mui/material";
 import { Xumm } from "xumm";
 import "./index.css";
+import { useState, useEffect } from "react";
 
-// const xumm = new Xumm('api-key')
+// require('Dotenv').config()
+
+const API_KEY = process.env.XUMM_API_KEY
+
+const xumm = new Xumm(API_KEY)
 
 export const NftMinter = () => {
+  const [account, setAccount] = useState(undefined)
+  const [file, setFile] = useState(undefined)
+
+  useEffect(() => {
+    xumm.on('success', async () => {
+      setAccount(await xumm.user.account)
+    })
+  }, [])
+
+  const connect = () => {
+    xumm.authorize()
+  }
+
+  const uploadImage = (e) => {
+    console.log("evnet", e)
+    const files = e.target.files
+    setFile(files[0])
+  }
+
   return (
     <div className="nft-minter-box">
       <div className="title">XRP NFT</div>
+        <div className="account-box">
+          <div className="account">{account}</div>
+            <Button variant="contained" onClick={connect}>
+              connect
+            </Button>
+        </div>
       <div className="image-box">
-        <Button variant="contained">
+        <Button variant="contained" onChange={uploadImage}>
           ファイルを選択
           <input
             className="imageInput"
@@ -18,6 +48,7 @@ export const NftMinter = () => {
           />
         </Button>
       </div>
+      {file && <img src={window.URL.createObjectURL(file)} alt="nft" className="nft-image" />}
     </div>
   );
 }
